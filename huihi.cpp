cee49030ca4cee77836fb6e4f8d2498e46d2a628
Damir10sorokin20 <114170327+Damir10sorokin20@users.noreply.github.com>
2022-11-03 18:36:08 +0300

    Add files via upload

---
+++ huihi.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//1
+	//int arr[5];
+	//ввод
+	//for (int i = 0; i < 5; i++) {
+		//cin >> arr[i];
+	//}
+		//вывод
+	//for (int i = 0; i < 5; i++) {
+		//cout << arr[i] << ' ';
+	//}
+
+	//2
+	//ввод
+	//int arr[i];
+	//for (int i = 0; i < 5; i++) {
+		//cin >> arr[i];
+	//}
+		//for (int i = 4; i >= 0; i--) {
+				//cout << arr[i] << ' ';
+			//}
+
+	//3
+	/*int arr[10];
+	for (int i = 0; i < 10; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < 10; i += 2) {
+		cout << arr[i] << ' ';
+	}*/
+
+// На оценку 
+//1
+ //int sum = 0;
+// int arr[10];
+ //for (int i = 0; i < 10; i++) {
+	 //cin >> arr[i];
+//}
+ //for (int i = 0; i < 10; i++) {
+	 //if (arr[i] > 0) {
+		 //sum = sum + 1;
+	 //}
+	 
+ //}
+ //cout << sum << ' ';
+
+	//2
+	//int arr[10];
+	//int sum = 0;
+	//int y = 0;
+	//for (int i = 0; i < 10; i++) {
+		//cin >> arr[i];
+	//}
+	//for (int i = 0; i < 10; i++) {
+		
+		//if (arr[i] % 2 != 0) {
+			//y = y + 1;
+		//}
+		//else if (arr[i] % 2 == 0) {
+		//sum = sum + 1;
+		//}
+	//}
+	//cout << sum << ' ' << y;
+
+
+
+
+
+
+
+
+
+
+}
